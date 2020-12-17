2a676257eb86c7fc2eec9ca38e429925a91a1211
ManhLe <manhle93@gmail.com>
2020-12-17 14:47:35 +0700

    fix

--- src/views/baocao/cuoingay.vue
+++ src/views/baocao/cuoingay.vue
@@ -18,9 +18,9 @@
       </el-col>
       <el-col :span="3">
         <el-select
-        @change="getData()"
+          @change="getData()"
           v-model="form.don_hang"
-          placeholder="Select"
+          placeholder="Loại đơn hàng"
           size="small"
           style="width: 100%"
           clearable
@@ -67,79 +67,78 @@
               : "Tổng doanh thu đặt hàng: "
           }}
         </span>
-        <span style="color: red;">  {{formate.formatCurrency(doanhThuDatHang)}}</span>
-          (VND)
+        <span style="color: red;">
+          {{ formate.formatCurrency(doanhThu) }}</span
+        >
+        (VND)
       </el-col>
       <el-col :span="5">
         <span style="color: green;">
           Số lượng đơn:
         </span>
-        <span style="color: red;">  {{formate.formatCurrency(doanhThuDatHang)}}</span>
-          (Đơn)
+        <span style="color: red;">
+          {{ formate.formatCurrency(total) }}</span
+        >
+        (Đơn)
       </el-col>
-      <el-col :span="5">
+      <!-- <el-col :span="5">
         <span style="color: green;">
           Số lượng sản phẩm
         </span>
-        <span style="color: red;">  {{formate.formatCurrency(doanhThuDatHang)}}</span>
-          (Sản phẩm)
-      </el-col>
-      <br>
-      <br>
+        <span style="color: red;">
+          {{ formate.formatCurrency(doanhThuDatHang) }}</span
+        >
+        (Sản phẩm)
+      </el-col> -->
+      <br />
+      <br />
     </el-row>
     <el-table
-      :data="form.don_hang == 'hoa_don' ? dataBanHang : dataDatHang"
+      :data="tableData"
       v-loading="tableLoading"
       border
       fit
       highlight-current-row
     >
-      <el-table-column
-        type="index" 
-        label="STT"
-        min-width="65"
-        align="center"
-        >
+      <el-table-column type="index" label="STT" min-width="65" align="center">
       </el-table-column>
-      <el-table-column
-        label="Mã HĐ"
-        min-width="65"
-        prop="ma"
-        >
+      <el-table-column label="Mã HĐ" min-width="65" prop="ma">
       </el-table-column>
       <el-table-column
         prop="ten"
         label="Tên hóa đơn"
         min-width="85"
       ></el-table-column>
-      <el-table-column
-        prop="so_luong"
-        label="Số lượng sản phẩm"
-        min-width="45"
-        >
+      <el-table-column prop="so_luong" label="Số lượng sản phẩm" min-width="45">
         <template slot-scope="scope">
           {{ formate.formatCurrency(scope.row.so_luong) + " "
           }}{{ scope.row.san_pham ? scope.row.san_pham.don_vi_tinh : "" }}
         </template>
       </el-table-column>
-      <el-table-column
-        prop="tong_tien" 
-        label="Thành tiền"
-        min-width="45"
-        >
+      <el-table-column prop="tong_tien" label="Thành tiền" min-width="45">
         <template slot-scope="scope">
-          {{ formate.formatCurrency(scope.row.tong_tien) + " đ"
-          }}
+          {{ formate.formatCurrency(scope.row.tong_tien) + " đ" }}
         </template>
       </el-table-column>
-      <el-table-column 
-        prop="updated_at" 
+      <el-table-column
+        prop="updated_at"
         label="Thời gian"
         min-width="45"
         align="center"
-        >
+      >
       </el-table-column>
     </el-table>
+    <br>
+    <div class="block">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        background
+        :page-sizes="[5, 10, 15, 20]"
+        layout="total, sizes, prev, pager, next"
+        :total="total"
+      ></el-pagination>
+    </div>
   </div>
 </template>
 <script>
@@ -150,47 +149,59 @@ export default {
     form: {
       date: [new Date(), new Date()],
       orderBy: null,
-      don_hang: null,
+      don_hang: "hoa_don"
     },
-    dataBanHang: [],
-    dataDatHang: [],
+    page: 1,
+    per_page: 10,
+    total: 0,
+    tableData: [],
+    doanhThu: 0,
     formate: formate,
     tableLoading: false,
-    doanhThuBanHang: 0,
-    doanhThuDatHang: 0,
   }),
   created() {
     this.getData();
   },
   methods: {
+    handleCurrentChange(val) {
+      this.page = val;
+      this.getData();
+    },
+    handleSizeChange(val) {
+      this.per_page = val;
+      this.getData();
+    },
     async getData() {
       this.tableLoading = true;
-      let data = await baoCaoCuoiNgay(this.form);
-      this.dataBanHang = data.data;
-      this.dataDatHang = data.data;
+      let data = await baoCaoCuoiNgay({
+        page: this.page,
+        per_page: this.per_page,
+        date: this.form.date,
+        don_hang: this.form.don_hang
+      });
+      this.doanhThu = data.tong_tien
+      this.per_page = data.data.per_page;
+      this.page = data.data.page;
+      this.tableData = data.data.data;
+      this.total = data.data.total;
       this.tableLoading = false;
-      this.doanhThuBanHang = 0;
-      this.doanhThuDatHang = 0;
-      this.dataBanHang.forEach(
-        (el) =>
-          (this.doanhThuBanHang =
-            +this.doanhThuBanHang + el.gia_ban * el.so_luong)
-      );
-      this.dataDatHang.forEach(
-        (el) =>
-          (this.doanhThuDatHang =
-            +this.doanhThuDatHang + el.gia_ban * el.so_luong)
-      );
     },
     taiBaoCao() {
-      let start = new Date(this.form.date[0]).toISOString()
-      let end = new Date(this.form.date[1]).toISOString()
+      let start = new Date(this.form.date[0]).toISOString();
+      let end = new Date(this.form.date[1]).toISOString();
       window.location.assign(
         process.env.VUE_APP_BASE_API +
-          "downloadbaocaobanhang?date[]=" +start + "&date[]=" + end + "&orderBy=" + this.form.orderBy + "&don_hang="+this.form.don_hang);
-    },
-  },
+          "downloadbaocaobanhang?date[]=" +
+          start +
+          "&date[]=" +
+          end +
+          "&orderBy=" +
+          this.form.orderBy +
+          "&don_hang=" +
+          this.form.don_hang
+      );
+    }
+  }
 };
 </script>
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/baocao/hanghoa.vue
+++ src/views/baocao/hanghoa.vue
@@ -58,61 +58,56 @@
               : "Tổng doanh thu đặt hàng: "
           }}
         </span>
-        <span style="color: red;">  {{formate.formatCurrency(doanhThuDatHang)}}</span>
-          (VND)
+        <span style="color: red;"> {{ formate.formatCurrency(doanhThu) }}</span>
+        (VND)
       </el-col>
       <el-col :span="5">
         <span style="color: green;">
           Số lượng đơn:
         </span>
-        <span style="color: red;">  {{formate.formatCurrency(doanhThuDatHang)}}</span>
-          (Đơn)
+        <span style="color: red;"> {{ formate.formatCurrency(tongDon) }}</span>
+        (Đơn)
       </el-col>
       <el-col :span="5">
         <span style="color: green;">
           Số lượng sản phẩm
         </span>
-        <span style="color: red;">  {{formate.formatCurrency(doanhThuDatHang)}}</span>
-          (Sản phẩm)
+        <span style="color: red;">
+          {{ formate.formatCurrency(tongSanPham) }}</span
+        >
+        (Sản phẩm)
       </el-col>
-      <br>
-      <br>
+      <br />
+      <br />
     </el-row>
-    <el-table
-      :data="form.don_hang == 'hoa_don' ? dataBanHang : dataDatHang"
-      v-loading="tableLoading"
-      border
-      fit
-    >
+    <el-table :data="tableData" v-loading="tableLoading" border fit>
       <el-table-column type="index" label="STT"></el-table-column>
-      <el-table-column prop="created_at" label="Mã hàng hóa"></el-table-column>
+      <el-table-column prop="ma" label="Mã hàng hóa"></el-table-column>
       <el-table-column
-        prop="san_pham.ten_san_pham"
+        prop="ten_san_pham"
         label="Tên hàng hóa"
       ></el-table-column>
-      <el-table-column prop="gia_ban" label="số lượng bán">
-        <template slot-scope="scope">
-          {{ formate.formatCurrency(scope.row.gia_ban) + " đ/ "
-          }}{{ scope.row.san_pham ? scope.row.san_pham.don_vi_tinh : "" }}
-        </template>
+      <el-table-column prop="so_luong_ban" label="số lượng bán">
       </el-table-column>
-      <el-table-column prop="so_luong" label="Số lượng trả">
-        <template slot-scope="scope">
-          {{ formate.formatCurrency(scope.row.so_luong) + " "
-          }}{{ scope.row.san_pham ? scope.row.san_pham.don_vi_tinh : "" }}
-        </template>
+      <el-table-column prop="so_luong_tra" label="Số lượng trả">
       </el-table-column>
       <el-table-column label="Doanh thu">
         <template slot-scope="scope">
-          {{
-            scope.row.doanh_thu
-              ? formate.formatCurrency(scope.row.doanh_thu) + " đ"
-              : formate.formatCurrency(scope.row.gia_ban * scope.row.so_luong) +
-                " đ"
-          }}
+          {{ formate.formatCurrency(scope.row.doanh_thu) }}
         </template>
       </el-table-column>
     </el-table>
+    <br>
+    <div class="block">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        background
+        :page-sizes="[5, 10, 15, 20]"
+        layout="total, sizes, prev, pager, next"
+        :total="total"
+      ></el-pagination>
+    </div>
   </div>
 </template>
 <script>
@@ -125,53 +120,67 @@ export default {
       date: [new Date(), new Date()],
       orderBy: null,
       don_hang: null,
-      danh_muc_id: null,
+      danh_muc_id: null
     },
-    dataBanHang: [],
-    dataDatHang: [],
+    page: 1,
+    per_page: 10,
+    total: 0,
+    tableData: [],
     formate: formate,
     tableLoading: false,
-    doanhThuBanHang: 0,
-    doanhThuDatHang: 0,
     danhMucs: [],
+    tongDon: 0,
+    doanhThu: 0,
+    tongSanPham: 0
   }),
   created() {
     this.getData();
     this.getDanhMuc();
   },
   methods: {
+    handleCurrentChange(val) {
+      this.page = val;
+      this.getData();
+    },
+    handleSizeChange(val) {
+      this.per_page = val;
+      this.getData();
+    },
     async getData() {
       this.tableLoading = true;
-      let data = await baoCaoHangHoa(this.form);
-      this.dataBanHang = data.data;
-      this.dataDatHang = data.data;
+      let data = await baoCaoHangHoa({
+        date: this.form.date,
+        danh_muc_id: this.form.danh_muc_id,
+        per_page: this.per_page,
+        page: this.page
+      });
+      this.tableData = data.data.data;
       this.tableLoading = false;
-      this.doanhThuBanHang = 0;
-      this.doanhThuDatHang = 0;
-      this.dataBanHang.forEach(
-        (el) =>
-          (this.doanhThuBanHang =
-            +this.doanhThuBanHang + el.gia_ban * el.so_luong)
-      );
-      this.dataDatHang.forEach(
-        (el) =>
-          (this.doanhThuDatHang =
-            +this.doanhThuDatHang + el.gia_ban * el.so_luong)
-      );
+      this.tongDon = data.tong_don;
+      this.doanhThu = data.doanh_thu;
+      this.tongSanPham = data.data.total;
+      this.total = data.data.total
     },
     async getDanhMuc() {
       let data = await index();
       this.danhMucs = data.data;
     },
     taiBaoCao() {
-      let start = new Date(this.form.date[0]).toISOString()
-      let end = new Date(this.form.date[1]).toISOString()
+      let start = new Date(this.form.date[0]).toISOString();
+      let end = new Date(this.form.date[1]).toISOString();
       window.location.assign(
         process.env.VUE_APP_BASE_API +
-          "downloadbaocaobanhang?date[]=" +start + "&date[]=" + end + "&orderBy=" + this.form.orderBy + "&don_hang="+this.form.don_hang);
-    },
-  },
+          "downloadbaocaobanhang?date[]=" +
+          start +
+          "&date[]=" +
+          end +
+          "&orderBy=" +
+          this.form.orderBy +
+          "&don_hang=" +
+          this.form.don_hang
+      );
+    }
+  }
 };
 </script>
-<style scoped>
-</style>
+<style scoped></style>
